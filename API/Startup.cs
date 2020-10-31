09393bed94b8fa1190917801ee77df069fb316c8
elifarmagan <elifs.armagan@gmail.com>
2020-10-31 15:56:20 +0300

    Added angular component

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,6 +23,7 @@ namespace API {
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +37,9 @@ namespace API {
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().
+            WithOrigins("https://localhost:4200") );
 
             app.UseAuthorization();
 
