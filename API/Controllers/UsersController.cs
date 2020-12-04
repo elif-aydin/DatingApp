dd7f7a4ad4aca112e3fc85da2e23a0026a750068
elifarmagan <elifs.armagan@gmail.com>
2020-12-05 00:22:54 +0300

    added user seed logic

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
@@ -9,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers {
 
+    [Authorize]
     public class UsersController : BaseApiController {
         private readonly DataContext _context;
         public UsersController(DataContext context) {
@@ -16,12 +16,10 @@ namespace API.Controllers {
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
             return await _context.Users.ToListAsync();
         }
 
-        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id) {
             return await _context.Users.FindAsync(id);
--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace API {
     public class Program {
-        public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
+        public static async Task Main(string[] args) {
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try {
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedUsers(context);
+            } catch (Exception exp) {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(exp, "An error occured during migration");
+            }
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
