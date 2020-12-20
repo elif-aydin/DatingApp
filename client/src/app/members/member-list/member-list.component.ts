507777351d162da96239b986ef6958d8eeb983d4
elifarmagan <elifs.armagan@gmail.com>
2020-12-20 18:50:40 +0300

    added filter buttons

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -13,11 +13,12 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-
   members: Member[];
   pagination: Pagination;
   userParams: UserParams;
   user: User;
+  genderList = [{ value: 'male', display: 'Males' },
+  { value: 'female', display: 'Females' }];
 
   constructor(private memberService: MembersService, private accountService: AccountService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
@@ -35,6 +36,11 @@ export class MemberListComponent implements OnInit {
       this.members = response.result;
       this.pagination = response.pagination;
     })
+  }
+
+  resetFilters() {
+    this.userParams = new UserParams(this.user);
+    this.loadMembers();
   }
 
   pageChanged(event: any) {
