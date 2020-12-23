452b4ad4a90c29acaae36213a4f429eed816d82b
elifarmagan <elifs.armagan@gmail.com>
2020-12-23 22:21:08 +0300

    finished message feature on the API side

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -12,9 +12,9 @@ namespace API.Interfaces {
 
         Task<Message> GetMessage(int id);
 
-        Task<PagedList<MessageDto>> GetMessageForUser();
+        Task<PagedList<MessageDto>> GetMessageForUser(MessageParams messageParams);
 
-        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+        Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername);
 
         Task<bool> SaveAllAsync();
     }
