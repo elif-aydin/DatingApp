1331906de6053475afb5a2a992ca3d035b4834b7
elifarmagan <elifs.armagan@gmail.com>
2020-12-26 22:52:32 +0300

    addin hub connection and tracking groups

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -19,12 +19,20 @@ namespace API.Data {
             _context = context;
         }
 
+        public void AddGroup(Group group) {
+            _context.Groups.Add(group);
+        }
+
         public void AddMessage(Message message) {
             _context.Messages.Add(message);
         }
 
         public void DeleteMessage(Message message) {
             _context.Messages.Remove(message);
+        }
+
+        public async Task<Connection> GetConnection(string connectionId) {
+            return await _context.Connections.FindAsync(connectionId);
         }
 
         public async Task<Message> GetMessage(int id) {
@@ -34,21 +42,26 @@ namespace API.Data {
                 .SingleOrDefaultAsync(x => x.Id == id);
         }
 
+        public async Task<Group> GetMessageGroup(string groupName) {
+            return await _context.Groups
+                .Include(x => x.Connections)
+                .FirstOrDefaultAsync(x => x.Name == groupName);
+        }
+
         public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams) {
             var query = _context.Messages
                 .OrderByDescending(m => m.MessageSent)
                 .AsQueryable();
 
             query = messageParams.Container switch {
-                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username 
+                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username
                     && u.RecipientDeleted == false),
-                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username  
+                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username
                     && u.SenderDeleted == false),
                 _ => query.Where(u => u.Recipient.UserName ==
                     messageParams.Username && u.RecipientDeleted == false && u.DateRead == null)
             };
             var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
-
             return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
@@ -60,7 +73,7 @@ namespace API.Data {
                 .Where(m => m.Recipient.UserName == currentUsername && m.RecipientDeleted == false
                         && m.Sender.UserName == recipientUsername
                         || m.Recipient.UserName == recipientUsername
-                        && m.Sender.UserName == currentUsername &&  m.SenderDeleted == false
+                        && m.Sender.UserName == currentUsername && m.SenderDeleted == false
                 )
                 .OrderBy(m => m.MessageSent)
                 .ToListAsync();
@@ -75,6 +88,10 @@ namespace API.Data {
                 await _context.SaveChangesAsync();
             }
             return _mapper.Map<IEnumerable<MessageDto>>(messages);
+        }
+
+        public void RemoveConnection(Connection connection) {
+            _context.Connections.Remove(connection);
         }
 
         public async Task<bool> SaveAllAsync() {
--- API/Data/Migrations/20201226194924_GroupsAdded.Designer.cs
+++ API/Data/Migrations/20201226194924_GroupsAdded.Designer.cs
@@ -3,12 +3,14 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data.Migrations {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot {
-        protected override void BuildModel(ModelBuilder modelBuilder) {
+    [Migration("20201226194924_GroupsAdded")]
+    partial class GroupsAdded {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder) {
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "5.0.0-rc.1.20451.13");
