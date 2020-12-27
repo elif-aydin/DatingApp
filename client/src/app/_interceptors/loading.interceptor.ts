a0923679a3af41ac8438b8701c028e6ed496b598
elifarmagan <elifs.armagan@gmail.com>
2020-12-27 22:41:29 +0300

    pre switch to postgres

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -1,23 +1,21 @@
-import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
+  HttpEvent, HttpHandler,
+
+  HttpInterceptor, HttpRequest
 } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { BusyService } from '../_services/busy.service';
-import { delay, finalize } from 'rxjs/operators';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
 
-  constructor(private busyService: BusyService) {}
+  constructor(private busyService: BusyService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.busyService.busy();
     return next.handle(request).pipe(
-      delay(1000),
       finalize(() => {
         this.busyService.idle();
       })
